2a0b851537b3c8a95eb7d16b104db1cc7beed510
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-19 15:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
 
-const fetchSpecifiedDrivers = function(){
+const fetchSpecifiedDrivers = function(someArray){
 
 }
