062e6d3c2f56b1e44640327da89ba32cfbeb8fad
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-19 14:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers(someArray) {
-  const firstTwo = [someArray[0], someArray[1]];
-  return firstTwo;
+const returnFirstTwoDriversfunction (someArray) {
+  return firstTwo = [someArray[0], someArray[1]];
+  
 }
 
 function returnLastTwoDrivers(){
